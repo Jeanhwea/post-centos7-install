5fb6f48b7678c73145af0cfa0f29d661dfc5fef9
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-08-18 20:14:33 +0800

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,6 @@
-#
+# 时间和时区相关操作
 timedatectl
+
 
 # 创建和使用 anaconda 的虚拟环境
 conda create --offline -n tf113
@@ -9,3 +10,10 @@ pip download -i https://pypi.tuna.tsinghua.edu.cn/simple -d wheels_tf113 tensorf
 pip download -i https://pypi.tuna.tsinghua.edu.cn/simple -d wheels_tf200 tensorflow-gpu==2.0.0b1
 
 pip install --no-index --find-link=wheels_tf113 tensorflow-gpu==1.13.1
+
+
+# 图形界面操作
+yum groupinstall "GNOME Desktop" "Graphical Administration Tools"
+systemctl get-default
+systemctl set-default graphical.target
+systemctl set-default multi-user.target
