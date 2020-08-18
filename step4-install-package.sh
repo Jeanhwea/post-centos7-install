ed8b52adf9bcb808fff9e6a90e8e4ee857fe03a9
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-08-18 19:23:34 +0800

    update

---
+++ step4-install-package.sh
@@ -0,0 +1,24 @@
+USERNAME=admin
+PACKAGES=~admin/Downloads
+
+################################################################################
+# java
+################################################################################
+mkdir -p /usr/local/java && \
+  cd /usr/local/java && \
+  tar xzvf $PACKAGES/jdk-8u191-linux-x64.tar.gz && \
+tar xzvf $PACKAGES/apache-maven-3.6.3-bin.tar.gz
+
+################################################################################
+# docker
+################################################################################
+cd /tmp && \
+  tar xzvf $PACKAGES/docker-ce-19.03.7.tar.gz && \
+  cd docker-ce-19.03.7 && \
+  yum localinstall *.rpm
+
+cp $PACKAGES/docker-compose /usr/local/bin/docker-compose && \
+  chmod +x /usr/local/bin/docker-compose
+
+systemctl enable docker.service
+usermod -G docker $USERNAME
