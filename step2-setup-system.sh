ed92ad637380f913251ae9174e61a4365912d495
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-09-18 18:44:55 +0800

    add

--- step2-setup-system.sh
+++ step2-setup-system.sh
@@ -1,10 +1,23 @@
 HERE=`cd $(dirname $0); pwd`
 
 cat >> /etc/profile <<\EOF
+# jdk & maven
 export JAVA_HOME=/usr/local/java/jdk1.8.0_191
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar${CLASSPATH:+:${CLASSPATH}}
 export MAVEN_HOME=/usr/local/java/apache-maven-3.6.3
 export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
+
+# python & anaconda
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONPATH=.
+export CONDA_HOME=/usr/local/anaconda3
+alias py='$CUDA_HOME/bin/python'
+alias ipy='$CUDA_HOME/bin/ipython'
+
+# tmux
+alias t='tmux list-sessions'
+alias ta='tmux attach -t Jinghui || tmux new-session -s Jinghui'
+alias td='tmux detach'
 
 # export PEIZHI_URL=http://172.21.0.10/peizhi
 
