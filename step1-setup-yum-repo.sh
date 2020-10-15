f4e77b4fd851c912ba7f34cc380f10626ca4adfe
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-10-15 16:35:45 +0800

    添加一些编译 mysql 的依赖包

--- step1-setup-yum-repo.sh
+++ step1-setup-yum-repo.sh
@@ -26,10 +26,11 @@ yum install -y binutils compat-libcap1 compat-libstdc++-33 gcc gcc-c++ glibc \
     glibc-devel ksh libgcc libstdc++ libstdc++-devel libaio libaio-devel libXi \
     libXtst make sysstat unixODBC unixODBC-devel oracleasm-support
 
-yum install -y cmake automake zip unzip python3 python3-devel python-devel python \
-    rpm-build redhat-rpm-config asciidoc hmaccalc perl-ExtUtils-Embed pesign xmlto \
-    audit-libs-devel binutils-devel elfutils-devel elfutils-libelf-devel ncurses-devel \
-    newt-devel numactl-devel pciutils-devel python-devel zlib-devel readline-devel
+yum install -y gcc gcc-c++ cmake automake zip unzip python3 python3-devel \
+    python-devel python rpm-build redhat-rpm-config asciidoc hmaccalc \
+    perl-ExtUtils-Embed pesign xmlto audit-libs-devel binutils-devel \
+    elfutils-devel elfutils-libelf-devel ncurses-devel bison-devel newt-devel \
+    numactl-devel pciutils-devel python-devel zlib-devel readline-devel
 
 # for cuda install
 # yum install -y python* python3* mesa* freeglut* *glew*
