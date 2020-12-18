c4b33005ca99d52cbb7afa7180f884fbeac3fc52
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-12-18 21:36:35 +0800

    add

--- step1-setup-yum-repo.sh
+++ step1-setup-yum-repo.sh
@@ -19,7 +19,7 @@ mkdir -p /media/cdrom && mount /dev/cdrom /media/cdrom &&
 yum makecache
 
 # common tools
-yum install -y net-tools tmux ntp kernel-devel kernel-doc kernel-headers
+yum install -y net-tools tmux ntp tree kernel-devel kernel-doc kernel-headers
 
 # for oracle 11g install
 yum install -y binutils compat-libcap1 compat-libstdc++-33 gcc gcc-c++ glibc \
@@ -31,6 +31,10 @@ yum install -y gcc gcc-c++ cmake automake zip unzip python3 python3-devel \
     perl-ExtUtils-Embed pesign xmlto audit-libs-devel binutils-devel \
     elfutils-devel elfutils-libelf-devel ncurses-devel bison-devel newt-devel \
     numactl-devel pciutils-devel python-devel zlib-devel readline-devel
+
+# for mysql
+yum install -y gcc gcc-c++ cmake ncurses ncurses-devel bison bison-devel openssl \
+    openssl-devel
 
 # for cuda install
 # yum install -y python* python3* mesa* freeglut* *glew*
