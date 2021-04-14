a3e723bc2ba086e766adb42369516979111c3dd9
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-14 08:05:23 +0800

    add

--- step5-bashrc-envs.sh
+++ step5-bashrc-envs.sh
@@ -1,9 +1,11 @@
-# User specific aliases and functions
+HERE=`cd $(dirname $0); pwd`
+CONFFILE=~/.bashrc
 
-sed -i '/# User specific aliases and functions/a#<>#' ~/.bashrc
-sed -i '/#<>#/,$d' ~/.bashrc
+sed -i '/# User specific aliases and functions/a#<>#' $CONFFILE
+sed -i '/#<>#/,$d' $CONFFILE
+echo "# Last updated at $(date +'%Y-%m-%d')" >> $CONFFILE
 
-cat >> ~/.bashrc <<\EOF
+cat >> $CONFFILE <<\EOF
 export PS1='[\u@\h \w]\$ '
 alias db='rlwrap sqlplus bamtri_mes/bamtri_mes'
 alias de='sqlplus -S bamtri_mes/bamtri_mes'
