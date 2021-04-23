7f86576570686d8cb8069fc096107860034e7ce6
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-24 07:25:43 +0800

    add

--- step5-bashrc-envs.sh
+++ step5-bashrc-envs.sh
@@ -6,7 +6,7 @@ sed -i '/#<>#/,$d' $CONFFILE
 echo "# Last updated at $(date +'%Y-%m-%d'), DO NOT ADD SCRIPT UNDER THIS LINE!!!" >> $CONFFILE
 
 cat >> $CONFFILE <<\EOF
-export PS1='[\u@\h \w]\$ '
+export PS1='[\u@\033[31m\h\033[0m \033[33m\w\033[0m]\$ '
 alias down='find . -maxdepth 3 -name sc | xargs -I {} bash -c "{} s"'
 alias db='rlwrap sqlplus bamtri_mes/bamtri_mes'
 alias de='sqlplus -S bamtri_mes/bamtri_mes'
