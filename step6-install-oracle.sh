fc7f14efecca0821ed06c9c54c381c1a69e5197b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-25 00:53:52 +0800

    update

--- step6-install-oracle.sh
+++ step6-install-oracle.sh
@@ -22,11 +22,14 @@ if [ ! -f $INSTALLER ]; then
   echo "Error: $INSTALLER not found!"
   exit 2
 fi
-logi "Installing Oracle Database 11g ..."
 
+logi "Installing Oracle Database 11g ..."
 
 sed "s/orclmem/${ORCLMEM}/g" ./response/db_install.rsp > /tmp/db_install.rsp \
   && chmod 777 ./response/db_install.rsp
+
+sleep 5 && \
+  sudo su - oracle -c "tail -f /u01/app/oraInventory/logs/installActions*.log"
 
 sleep 120 && \
   sudo su - oracle -c "sed -i 's/^\(\s*$(MK_EMAGENT_NMECTL)\)\s*$/\1 -lnnz11/g' $ORACLE_HOME/sysman/lib/ins_emagent.mk" &
