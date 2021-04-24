93aeb7800fb6dcd068168e23c9278959ccfbfe54
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-24 10:33:26 +0800

    polish PS1

--- step5-bashrc-envs.sh
+++ step5-bashrc-envs.sh
@@ -22,7 +22,7 @@ echo "# Last updated at $(date +'%Y-%m-%d'), DO NOT ADD SCRIPT UNDER THIS LINE!!
 
 # add with environment substitute
 cat >> $CONFFILE << EOF
-export PS1='[${CLRHOST}\u@\h${CLRRST} ${CLRBLU}\w${CLRRST}]\$ '
+export PS1='[${CLRHOST}\u@\h${CLRRST} ${CLRBLU}\w${CLRRST}]${CLRBLU}\$${CLRRST} '
 EOF
 
 # add without environment substitute
