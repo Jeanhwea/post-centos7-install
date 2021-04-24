9ecdd63e70d859de28243170ed7693038c7f3238
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-24 22:10:14 +0800

    add: install oracle

--- step6-install-oracle.sh
+++ step6-install-oracle.sh
@@ -1,5 +1,7 @@
 HERE=`cd $(dirname $0); pwd`
 INSTALLER=/media/cdrom/database/runInstaller
+SYSUSER=${SYSUSER:="system"}
+SYSPASS=${SYSPASS:="oracle"}
 
 sudo mount /dev/cdrom /media/cdrom
 
@@ -16,3 +18,29 @@ sudo su - oracle -c "$INSTALLER -silent -ignorePrereq -waitforcompletion -respon
 echo "Executing root scripts"
 sudo /u01/app/oraInventory/orainstRoot.sh
 sudo /u01/app/oracle/product/11.2.0/dbhome_1/root.sh
+
+echo "Post install tasks"
+sudo sed -i 's/# su - oracle/su - oracle/g' /etc/rc.local
+sudo sed -i 's/:N$/:Y/g' /etc/oratab
+
+sudo su - oracle -c "cd /u01/app/oracle/oradata && mkdir mes && mkdir spot"
+
+
+read -d '' ORACLE_POST_SETUP_SQL << EOF
+--------------------------------------------------------------------------------
+-- sysplus / as sysdba
+--------------------------------------------------------------------------------
+alter system set processes = 1500 scope = spfile;
+alter system set sessions = 2272 scope = spfile;
+alter system set transactions = 1650 scope = spfile;
+alter system set deferred_segment_creation = false scope = both;
+
+--------------------------------------------------------------------------------
+-- profiles
+--------------------------------------------------------------------------------
+-- select * from dba_profiles;
+alter profile default limit failed_login_attempts unlimited;
+alter profile default limit password_life_time unlimited;
+EOF
+
+echo "$ORACLE_POST_SETUP_SQL" | sqlplus -S ${SYSUSER}/${SYSPASS}
