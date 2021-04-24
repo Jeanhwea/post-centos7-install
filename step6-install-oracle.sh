69042805a5d3de265ebd453a3f9b8e66b7c9920c
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-25 00:43:33 +0800

    update

--- step6-install-oracle.sh
+++ step6-install-oracle.sh
@@ -12,33 +12,34 @@ CLRBLU="\033[34m"
 CLRMGA="\033[35m"
 CLRRST="\033[0m"
 
+logi() {
+  echo -e "$(date +'%F %T : ') ${CLRYLW}$*${CLRRST}"
+}
+
 sudo mount /dev/cdrom /media/cdrom
 
 if [ ! -f $INSTALLER ]; then
   echo "Error: $INSTALLER not found!"
   exit 2
 fi
+logi "Installing Oracle Database 11g ..."
 
-logi() {
-  echo -e "$(date +'%F %T : ') ${CLRYLW}$*${CLRRST}"
-}
 
 sed "s/orclmem/${ORCLMEM}/g" ./response/db_install.rsp > /tmp/db_install.rsp \
   && chmod 777 ./response/db_install.rsp
 
-
 sudo su - oracle -c "sleep 200 && sed -i 's/^\(\s*$(MK_EMAGENT_NMECTL)\)\s*$/\1 -lnnz11/g' $ORACLE_HOME/sysman/lib/ins_emagent.mk &"
 
 
-logi "Installing Oracle Database 11g ..."
+logi "Invoking Oracle Database 11g ..."
 sudo su - oracle -c "$INSTALLER -silent -ignorePrereq -waitforcompletion -responseFile /tmp/db_install.rsp"
 
 
-logi "Executing root scripts"
+logi "Executing root scripts ..."
 sudo su - root -c "/u01/app/oraInventory/orainstRoot.sh"
 sudo su - root -c "/u01/app/oracle/product/11.2.0/dbhome_1/root.sh"
 
-logi "Executing Post-install tasks"
+logi "Executing Post-install tasks ..."
 sudo sed -i 's/:N$/:Y/g' /etc/oratab
 sudo sed -i 's/# su - oracle/su - oracle/g' /etc/rc.d/rc.local
 sudo chmod +x /etc/rc.d/rc.local
