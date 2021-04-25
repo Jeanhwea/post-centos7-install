6799fe93b64a23a26d478a5e18f33eff9016e758
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-25 11:39:51 +0800

    update

--- ins02-oracle-database.sh
+++ ins02-oracle-database.sh
@@ -13,11 +13,11 @@ logi() {echo -e "$(date +'%F %T : ') ${CLRGRN}$*${CLRRST}"}
 logw() {echo -e "$(date +'%F %T : ') ${CLRYLW}$*${CLRRST}"}
 loge() {echo -e "$(date +'%F %T : ') ${CLRRED}$*${CLRRST}"}
 
-su - root  -c "$HERE/step6-install-oracle.sh"
+su - root  -c "$HERE/step31-install-oracle.sh"
 
 su - oracle -c "cd /u01/app/oracle/oradata && mkdir mes && mkdir spot"
 
 for query in $(ls "$HERE/queries/*.sql"); do
   logi "Execute file $query"
-  sqlplus $SYSUSER/$SYSPASS < $query
+  sqlplus -S $SYSUSER/$SYSPASS < $query
 done
