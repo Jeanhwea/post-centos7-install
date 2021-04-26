887c45306ace1ed857bc78c01581697d1bb8f31a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-26 20:28:10 +0800

    update

--- step31-install-oracle.sh
+++ step31-install-oracle.sh
@@ -21,7 +21,7 @@ loge() { echo -e "$(date +'%F %T : ') ${CLRRED}$*${CLRRST}"; }
 runw() { sh -c "$*" | while read line; do echo -e "${CLRYLW}$line${CLRRST}"; done; }
 
 runw 'df -h'
-logi "Allocate ${TOTALMEM}M Memory for oracle."
+logi "Allocate Memory ${ORCLMEM}M of ${TOTALMEM}M for oracle database."
 runw 'free'
 
 mkdir -p /media/cdrom
