af4bd29898ed89394fa01026b5bd76e7e0bcb31b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-05-12 13:54:55 +0800

    add install pg script

---
+++ step42-install-postgres.sh
@@ -0,0 +1,93 @@
+HERE=`cd $(dirname $0); pwd`
+USERNAME=admin
+PACKAGES=~admin/download/packages
+PGHOME=/usr/local/pgsql
+PGDATA=/usr/local/pgsql/data
+
+CLRRED="\033[31m"
+CLRGRN="\033[32m"
+CLRYLW="\033[33m"
+CLRBLU="\033[34m"
+CLRMGA="\033[35m"
+CLRRST="\033[0m"
+
+logi() { echo -e "$(date +'%F %T : ') ${CLRGRN}$*${CLRRST}"; }
+logw() { echo -e "$(date +'%F %T : ') ${CLRYLW}$*${CLRRST}"; }
+loge() { echo -e "$(date +'%F %T : ') ${CLRRED}$*${CLRRST}"; }
+
+if [ ! -f $PACKAGES/postgresql-9.6.19.tar.gz ]; then
+  loge "Error: $PACKAGES/postgresql-9.6.19.tar.gz not found!"
+  exit 2
+fi
+
+logi "Extract package to $PGHOME ..."
+groupadd postgres && \
+  useradd -r -g postgres postgres
+
+cd /usr/local && \
+  tar xzf $PACKAGES/postgresql-9.6.19.tar.gz && \
+  chown -R postgres:postgres postgresql-9.6.19 && \
+  ln -s postgresql-9.6.19 pgsql
+
+mkdir -p $PGHOME/data && \
+  chown -R postgres:postgres $PGHOME/data
+
+
+logi "Configuring, Building and Install ..."
+cd $PGHOME && \
+  ./configure --prefix=$PGHOME && \
+  make world && \
+  make install-world
+
+
+logi "Initializing Database ..."
+
+su - postgres -c "$PGHOME/bin/initdb -E UTF8 -D $PGDATA"
+su - postgres -c "$PGHOME/bin/pg_ctl -D $PGDATA -l logfile start"
+
+# if [ -f /etc/my.cnf ] && [ ! -f /etc/my.cnf.1 ]; then
+#   logw "Backup /etc/my.cnf"
+# fi
+
+
+logi "Starting mysqld service ..."
+service mysql.server start
+
+cat >> /etc/rc.d/rc.local << EOF
+################################################################################
+# postgres
+################################################################################
+su - postgres -c "/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start"
+EOF
+chmod +x /etc/rc.d/rc.local
+
+cat >> /etc/profile <<\EOF
+# postgres basic envs
+export PGHOME=/usr/local/pgsql
+export PGDATA=/usr/local/pgsql/data
+export PATH=$PGHOME/bin:$PATH
+# postgres user and port
+export PGUSER=postgres
+export PGPORT=5432
+
+# alias for postgres
+alias cdph="cd $PGHOME"
+alias p0="$PGHOME/bin/pg_ctl stop"
+alias p1="$PGHOME/bin/pg_ctl -D $PGDATA -l $PGDATA/logfile start"
+EOF
+
+read -d '' -r PG_CHEATSHEET_STR << EOF
+-- Login with:
+
+  sudo su - postgres
+  $PGHOME/bin/pgsql
+
+-- Create database and user using:
+
+  /usr/local/pgsql/bin/createdb -E UTF8 demo
+  /usr/local/pgsql/bin/psql demo
+
+EOF
+
+logw "Cheatsheet :)"
+echo "$PG_CHEATSHEET_STR"
